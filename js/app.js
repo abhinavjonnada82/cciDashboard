604545ae64413f35124fbc7bde481b1c97f1448a
Abhinav <abhinav.jonnada@gmail.com>
2020-11-17 12:40:19 -0500

    added age group chart

--- js/app.js
+++ js/app.js
@@ -190,7 +190,56 @@ $(document).ready(function(){
 
   function processData(data) {
     console.log('process', data)
-    targetAgeGroupDataProcess(data)
-    targetGenderDataProcess(data)
+    // targetAgeGroupDataProcess(data)
+    // targetGenderDataProcess(data)
+  }
+
+  function targetAgeGroupDataProcess(data) {
+    ageGroupHolder = [0,0,0]
+    for (i = 0; i < data.length; i++) {
+      if (data[i].age_group === '30 to 49') {
+        ageGroupHolder[0] += 1
+      }
+      else if (data[i].age_group === '50 to 69') {
+        ageGroupHolder[1] += 1
+      }
+      else {
+        ageGroupHolder[2] += 1
+      }
+    }
+  
+  let ctx = $('#myChart');
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['30 to 49', '50 to 69', '70 or Older'],
+        datasets: [{
+            label: 'Age group',
+            data: ageGroupHolder,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+console.log('mc!', myChart)
   }
 })
