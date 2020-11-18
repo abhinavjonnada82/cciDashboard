fc565bf4c854f4d67c275359ffdd7a67c1ce4dd8
Abhinav <abhinav.jonnada@gmail.com>
2020-11-17 19:36:37 -0500

    minor fixes

--- js/app.js
+++ js/app.js
@@ -1,28 +1,7 @@
-
-// const AUTH_TOKEN = 'LP84l7c8EfxIN4x3xJE5cfZjI';
-// const SECRET_TOKEN = 'PRDWsMDGmw6jgBvTR-rLOIL2GjC7acZfJtM4';
-// const baseURL = 'https://soda.demo.socrata.com/oauth/access_token'
-// const redirectUri = 'https://localhost:8080/callback'
+// With the help of Chart.js, charts were implemented. Code structure, on load the Fetch makes a GET request to REST API
+//  Using the data obtained, analysis are performed on each specifc category such as gender, cancer type, age group & more.
 
 $(document).ready(function(){
-  // $("button").click(function(){
-  // let url = baseURL + '?client_id' + AUTH_TOKEN + '&client_secret' + SECRET_TOKEN + '&grant_type=authorization_code'+ '&redirect_uri=' + redirectUri;
-  // //  let url = `https://health.data.ny.gov/resource/gnzp-ekau.json?$where=UPPER(ccs_diagnosis_description) like '%25CANCER%25'&$limit=10`;
-  // // message: "No such app token"
-  //   console.log('url', url)
-  //   fetch(url, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   }).then(function(result) {
-  //     console.log('url', result)
-  //     return result.json();
-  // }).then(function(json) {
-  //     console.log('json', json);
-  // });
-  // });
-
     let url = `https://health.data.ny.gov/resource/gnzp-ekau.json?$where=UPPER(ccs_diagnosis_description) like '%25CANCER%25'&$limit=30`;
       fetch(url, {
         method: 'GET',
@@ -32,13 +11,11 @@ $(document).ready(function(){
       }).then(function(result) {
         return result.json();
     }).then(function(json) {
-        console.log('json', json);
         processData(json)
     });
  
 
   function processData(data) {
-    console.log('process', data)
     getStatsDataProcess(data)
     getAgeGroupDataProcess(data)
     getGenderDataProcess(data)
@@ -47,29 +24,28 @@ $(document).ready(function(){
     getCancerTypeDataProcess(data)
   }
 
+// Function to get stats
   function getStatsDataProcess(data) {
-    $(".content .value").html(data.length);
+    $(".content .numPatients").html(data.length);
     totalCostHolder = 0
     for (i = 0; i < data.length; i++) {
-      console.log('tc22',(data[i].total_costs))
       totalCostHolder = totalCostHolder + parseFloat((data[i].total_costs))
     }
-    console.log('tc',totalCostHolder)
-    $(".content .value1").html(totalCostHolder);
+    $(".content .totalCosts").html(totalCostHolder);
 
   }
-
+// Function to display a chart for various age groups
   function getAgeGroupDataProcess(data) {
     ageGroupHolder = [0,0,0]
     for (i = 0; i < data.length; i++) {
       if (data[i].age_group === '30 to 49') {
-        ageGroupHolder[0] += 1
+        ageGroupHolder[0]++;
       }
       else if (data[i].age_group === '50 to 69') {
-        ageGroupHolder[1] += 1
+        ageGroupHolder[1]++;
       }
       else {
-        ageGroupHolder[2] += 1
+        ageGroupHolder[2]++;
       }
     }
   
@@ -109,14 +85,15 @@ $(document).ready(function(){
 
 }
 
+// Function to display a pie chart for gender specifc
   function getGenderDataProcess(data) {
     genderHolder = [0,0]
     for (i = 0; i < data.length; i++) {
       if (data[i].gender === 'F') {
-        genderHolder[0] += 1
+        genderHolder[0]++;
       }
       else if (data[i].gender === 'M') {
-        genderHolder[1] += 1
+        genderHolder[1]++;
       }
   }
     let ctx1 = $('#myChart1');
@@ -142,36 +119,40 @@ $(document).ready(function(){
   });
   }
 
+  // Function to display a radar chart for different insurance
   function getInsuranceDataProcess(data) {
     insuranceHolder = [0,0,0,0]
     secondaryInsuranceHolder = [0,0,0,0]
     for (i = 0; i < data.length; i++) {
       if (data[i].payment_typology_1 === 'Medicare') {
-        insuranceHolder[0] += 1
+        insuranceHolder[0]++;
       }
       else if (data[i].payment_typology_1 === 'Blue Cross/Blue Shield') {
-        insuranceHolder[1] += 1
+        insuranceHolder[1]++;
       }
       else if (data[i].payment_typology_1 === 'Private Health Insurance') {
-        insuranceHolder[2] += 1
+        insuranceHolder[2]++;
       }
       else {
-        insuranceHolder[3] += 1
+        insuranceHolder[3]++;
       }
   }
-  for (i = 0; i < data.length; i++) {
-    if (data[i].payment_typology_2 && data[i].payment_typology_2 === 'Medicare') {
-      secondaryInsuranceHolder[0] += 1
-    }
-    else if (data[i].payment_typology_2 && data[i].payment_typology_2 === 'Blue Cross/Blue Shield') {
-      secondaryInsuranceHolder[1] += 1
-    }
-    else if (data[i].payment_typology_2 && data[i].payment_typology_2 === 'Private Health Insurance') {
-      secondaryInsuranceHolder[2] += 1
-    }
-    else {
-      secondaryInsuranceHolder[3] += 1
-    }
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].payment_typology_2 !== null) {
+        if (data[i].payment_typology_2 === 'Medicare') {
+          secondaryInsuranceHolder[0]++;
+        }
+        else if (data[i].payment_typology_2 === 'Blue Cross/Blue Shield') {
+          secondaryInsuranceHolder[1]++;
+        }
+        else if (data[i].payment_typology_2 === 'Private Health Insurance') {
+          secondaryInsuranceHolder[2]++;
+        }
+        else {
+          secondaryInsuranceHolder[3]++;
+        }
+      }
 }
   let ctx2 = $('#myChart2');
   var myRadarChart = new Chart(ctx2, {
@@ -209,14 +190,16 @@ $(document).ready(function(){
 });
  }
 
+
+// Function to display a doughnut chart for different entry
 function getAdmissionDataProcess(data) {
     admissionHolder = [0,0]
     for (i = 0; i < data.length; i++) {
       if (data[i].type_of_admission === 'Emergency') {
-        admissionHolder[0] += 1
+        admissionHolder[0]++;
       }
       else if (data[i].type_of_admission === 'Elective') {
-        admissionHolder[1] += 1
+        admissionHolder[1]++;
       }
   }
 
@@ -243,26 +226,37 @@ function getAdmissionDataProcess(data) {
 });
 }
 
+// Function to display a bar chart for different cancer types
 function getCancerTypeDataProcess(data) {
+
+  // cancerTypeHolder = []
+  // cancerTypes = {'Cancer of colon': 0, 'Cancer of pancreas': 1, 'Cancer of stomach': 2, 'Cancer of cervix': 3, 'Cancer of bronchus lung': 4}
+  // for (i = 0; i <=  data.length; i++) {   
+  //   let val = data[i].ccs_diagnosis_description;
+  //   console.log('val', val)   
+  //   if(cancerTypes[val]) {   	
+  //     console.log('key',cancerTypes[val])
+  //     cancerTypeHolder[cancerTypes[val]]++;   
+  //   } 
   cancerTypeHolder = [0,0,0,0,0,0]
   for (i = 0; i < data.length; i++) {
     if (data[i].ccs_diagnosis_description === 'Cancer of colon') {
-      cancerTypeHolder[0] += 1
+      cancerTypeHolder[0]++;
     }
     else if (data[i].ccs_diagnosis_description === 'Cancer of pancreas') {
-      cancerTypeHolder[1] += 1
+      cancerTypeHolder[1]++;
     }
     else if (data[i].ccs_diagnosis_description === 'Cancer of stomach') {
-      cancerTypeHolder[2] += 1
+      cancerTypeHolder[2]++;
     }
     else if (data[i].ccs_diagnosis_description === 'Cancer of cervix') {
-      cancerTypeHolder[3] += 1
+      cancerTypeHolder[3]++;
     }
     else if (data[i].ccs_diagnosis_description === 'Cancer of bronchus; lung') {
-      cancerTypeHolder[4] += 1
+      cancerTypeHolder[4]++;
     }
     else {
-      cancerTypeHolder[5] += 1
+      cancerTypeHolder[5]++;
     }
 }
   let ctx4 = $('#myChart4');
